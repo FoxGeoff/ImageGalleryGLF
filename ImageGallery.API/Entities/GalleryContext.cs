60faf336baff65125e40f08e85ffdc814a5bfbb9
FoxGeoff <fox.geoffrey@gmail.com>
2018-06-27 15:51:10 -0700

    Add Initial ImageGallery.API project with migrations

---
+++ ImageGallery.API/Entities/GalleryContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageGallery.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageGallery.API.Entitiies
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext(DbContextOptions<GalleryContext> options)
+            :base(options)
+        { }
+
+        public DbSet<Image> Images { get; set; }
+    }
+}
---
+++ ImageGallery.API/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageGallery.API.Entitiies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ImageGallery.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+           _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // register the DbContext on the container, getting the connection string from
+            // appSettings (note: use this during development; in a production environment,
+            // it's better to store the connection string in an environment variable)
+            // note: It is more secure in development to use secrets.json through "Manage User Secrets"
+            services.AddDbContext<GalleryContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("imageGalleryDBConnectionString"));
+            });
+
+            //TODO: services.AddTransient<IMailService, NullMailService>();
+
+            //TODO: services.AddTransient<ImageSeeder>();
+
+            //TODO: services.AddScoped<IGalleryRepository, GalleryRepository>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
