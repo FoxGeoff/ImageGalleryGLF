4210371e8f9203855fc6c71c61db173c70e3985a
FoxGeoff <fox.geoffrey@gmail.com>
2019-08-27 13:01:22 -0700

    Add GetClients() credetials

--- Mavreak.IDP/Config.cs
+++ Mavreak.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
@@ -53,10 +54,29 @@ namespace Mavreak.IDP
 
         //TODO: API-related resouces (scopes)
 
-        //TODO: Code stub
+        //TODO: Code stub - DONE
         public static IEnumerable<Client> GetClients()
         {
-            return new List<Client>();
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    RedirectUris = new List<string>()
+                    {
+                        "https:localhost:44364/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
         }
     }
 }
